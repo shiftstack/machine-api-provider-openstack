b056f9ed5650ce3bad91a080d664c2d46f5456c1
Stephen Finucane <stephenfin@redhat.com>
2023-10-04 12:09:37 +0100

    WIP: Start implementing update validator
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>

--- api/v1beta1/machineset_webhook.go
+++ api/v1beta1/machineset_webhook.go
@@ -18,12 +18,17 @@ package v1beta1
 
 import (
 	"context"
+	"fmt"
+	"reflect"
 
 	"github.com/openshift/api/machine/v1beta1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/cluster-api/util/topology"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
 type MachineSetWebhook struct{}
@@ -43,6 +48,25 @@ func (r *MachineSetWebhook) ValidateCreate(ctx context.Context, obj runtime.Obje
 }
 
 func (r *MachineSetWebhook) ValidateUpdate(ctx context.Context, oldRaw runtime.Object, newRaw runtime.Object) error {
+	oldObj, ok := oldRaw.(*v1beta1.MachineSet)
+	if !ok {
+		return apierrors.NewBadRequest(fmt.Sprintf("expected an MachineSet but got a %T", oldRaw))
+	}
+
+	newObj, ok := newRaw.(*v1beta1.MachineSet)
+	if !ok {
+		return apierrors.NewBadRequest(fmt.Sprintf("expected an MachineSet but got a %T", oldRaw))
+	}
+
+	req, err := admission.RequestFromContext(ctx)
+	if err != nil {
+		return apierrors.NewBadRequest(fmt.Sprintf("expected an admission.Request inside context: %v", err))
+	}
+
+	if !topology.ShouldSkipImmutabilityChecks(req, newObj) && !reflect.DeepEqual(newObj, oldObj) {
+		return apierrors.NewBadRequest(fmt.Sprintf("MachineSet cannot be updated"))
+	}
+
 	return nil
 }
 
